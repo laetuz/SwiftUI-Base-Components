3c4e18cfd97738809be79ac3bc508d2efb110556
laetuz <laetuzg@gmail.com>
2024-02-14 23:05:49 +0700

    Initial Commit

---
+++ SwiftUI Base Components/SwiftUI_Base_ComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Base_ComponentsApp.swift
+//  SwiftUI Base Components
+//
+//  Created by Ryo Martin on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Base_ComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
